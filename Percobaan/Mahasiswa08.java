3f79ccc57b9333a32013a885585b816e7da0e1ef
Daffaazzi <azzidaffa@gmail.com>
2026-04-21 12:01:35 +0700

    Percobaan commit 1

---
+++ Percobaan/Mahasiswa08.java
@@ -0,0 +1,21 @@
+package Percobaan;
+public class Mahasiswa08 {
+    String nim;
+    String nama;
+    String kelas;
+    int nilai;
+
+    public Mahasiswa08() {
+    }
+
+    public Mahasiswa08(String nama, String nim, String kelas) {
+        this.nama = nama;
+        this.nim = nim;
+        this.kelas = kelas;
+        nilai = -1;
+    }
+
+    void tugasDinilai(int nilai) {
+        this.nilai = nilai;
+    }
+}
